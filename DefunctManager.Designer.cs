d9a6a3b6e520d888a11626fe383e9edd34774f32
ShootMe <birreller@gmail.com>
2016-04-15 13:43:46 -0400

    Label change

--- DefunctManager.Designer.cs
+++ DefunctManager.Designer.cs
@@ -66,9 +66,9 @@
 			this.lblCollectibles.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.lblCollectibles.Location = new System.Drawing.Point(3, 62);
 			this.lblCollectibles.Name = "lblCollectibles";
-			this.lblCollectibles.Size = new System.Drawing.Size(174, 20);
+			this.lblCollectibles.Size = new System.Drawing.Size(161, 20);
 			this.lblCollectibles.TabIndex = 9;
-			this.lblCollectibles.Text = "Collectibles: (0/2) (0/40)";
+			this.lblCollectibles.Text = "Collectibles: (0) (0/40)";
 			// 
 			// DefunctManager
 			// 
